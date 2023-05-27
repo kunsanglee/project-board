04bc76b471b7337234eca483ecac95eadd9511a4
kunsanglee <gosmdochee2@gmail.com>
2023-05-27 15:18:10 +0900

    #16 - spring boot 3.1.0 에서 2.7.0 으로 변경
    그에따른 각 파일의 import문 변경 및 build.gradle 변경

--- src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
